ebff14d19656991023cc4bad079f9f3fe851b737
Pratyashshrivastava <86169534+Pratyashshrivastava@users.noreply.github.com>
2024-06-05 23:55:23 +0530

    Create MaxNumberofKSumPairs.py

---
+++ MaxNumberofKSumPairs.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        l = 0
+        count = 0
+        r = len(nums)-1
+
+        while(l<r):
+            if nums[l]+nums[r] == k:
+                count+=1
+                
+                l+=1
+                r-=1
+            elif nums[l]+nums[r]<k:
+                l+=1
+            else:
+                r-=1
+        return count
+
+nums = [3,1,3,4,3]
+k = 6
+solution = Solution()
+
+print(solution.maxOperations(nums,k))
